f1a33d182bdb53091c5348dd369479283a39f775
BAMOGO <clement_bamogo@um5.ac.ma>
2024-01-02 20:07:51 +0100

    m

--- src/components/homeProject.jsx
+++ src/components/homeProject.jsx
@@ -1,6 +1,5 @@
 import { Link } from "react-router-dom"
 import "../css/homeProject.css"
-import { useEffect, useState } from "react";
 function HomeProject(props) {
     const styles = {
         content: {
@@ -11,23 +10,12 @@ function HomeProject(props) {
             borderRadius : 10
         },
     }
-
-    const [post, setPost] = useState(null);
-    useEffect(() => {
-        fetch("https://jsonplaceholder.typicode.com/posts/1")
-            .then((response) => response.json())
-            .then((response) => {
-                const { body } = response
-                console.log(body)
-                setPost(body)
-            })
-    }, []);
     return (
         <>
             <div style={styles.content}>
                 <Link  className="home-project-content" to={'/work'}>
                     <span style={styles.title}>{props.title}</span>
-                    {post ? post : <img style={styles.img} src={props.image} alt="Project 1" />}
+                    <img style={styles.img} src={props.image} alt="Project 1" />
                 </Link>
             </div>
         </>
